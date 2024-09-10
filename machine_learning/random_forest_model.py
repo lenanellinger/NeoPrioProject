62ff8461cc1adca069ce4dfdbea22d9b24f6842c
AHNellL1 <ahnelll1@SRV018.img.med.uni-tuebingen.de>
2024-09-10 10:40:08 +0200

    Permutation Feature Importance

--- machine_learning/random_forest_model.py
+++ machine_learning/random_forest_model.py
@@ -1,24 +1,39 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
 from sklearn.impute import SimpleImputer
+from sklearn.inspection import permutation_importance
+from sklearn import linear_model
+
+from scipy.cluster import hierarchy
+from scipy.spatial.distance import squareform
+from scipy.stats import spearmanr
 
 from data.get_data import get_feature_data
 
-features = get_feature_data()
+features_with_meta = get_feature_data()
 
-labels = np.array(features['response'])
+labels = np.array(features_with_meta['response'])
 
-features=features.loc[:, ['patientIdentifier', 'imputedGeneExpression', 'DAI', 'IEDB_Immunogenicity', 'MixMHCpred_score', 'PRIME_score', 'Selfsimilarity_conserved_binder', 'Tcell_predictor', 'dissimilarity_score', 'hex_alignment_score', 'recognition_potential']]
-features['Selfsimilarity_conserved_binder'] = features['Selfsimilarity_conserved_binder'].fillna(0)
+features_with_meta=features_with_meta.loc[:, ['patientIdentifier', 'DAI', 'IEDB_Immunogenicity', 'MixMHCpred_score', 'PRIME_score', 'Selfsimilarity_conserved_binder', 'Tcell_predictor', 'dissimilarity_score', 'hex_alignment_score', 'recognition_potential']]
+features_with_meta['Selfsimilarity_conserved_binder'] = features_with_meta['Selfsimilarity_conserved_binder'].fillna(0)
 
+features = features_with_meta.drop(['patientIdentifier'], axis=1)
 feature_list = list(features.columns)
-features_array = np.array(features)
+features_with_meta_array = np.array(features_with_meta)
 
-train_features, test_features, train_labels, test_labels = train_test_split(features_array, labels, test_size = 0.2, random_state = 42)
+train_features, test_features, train_labels, test_labels = train_test_split(features_with_meta_array, labels, test_size = 0.2, random_state = 42)
+
+train_features = train_features[:, 1:]
+train_labels_int = [0 if label == 'N' else 1 for label in train_labels]
+test_patients = test_features[:, 0]
+test_features = test_features[:, 1:]
+test_labels_int = [0 if label == 'N' else 1 for label in test_labels]
 
 imputer = SimpleImputer(strategy='median') 
 imputer.fit(train_features)
@@ -26,30 +41,160 @@ train_features = imputer.transform(train_features)
 test_features = imputer.transform(test_features)
 
 # TODO class imbalance
+# TODO overfitting
 
-train_features = train_features[:, 1:]
-
-rf_classifier = RandomForestClassifier(n_estimators=100, random_state=42)
+rf_classifier = RandomForestClassifier(n_estimators=100, random_state=42, min_samples_leaf=20)
 rf_classifier.fit(train_features, train_labels)
 
+clf = linear_model.Lasso(alpha=0.00001)
+clf.fit(train_features, train_labels_int)
+
+# Feature Correlation
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 8))
+corr = spearmanr(features).correlation
+
+corr = (corr + corr.T) / 2
+np.fill_diagonal(corr, 1)
+
+# We convert the correlation matrix to a distance matrix before performing
+# hierarchical clustering using Ward's linkage.
+distance_matrix = 1 - np.abs(corr)
+distance_matrix = np.nan_to_num(distance_matrix, nan=1)
+dist_linkage = hierarchy.ward(squareform(distance_matrix))
+dendro = hierarchy.dendrogram(
+    dist_linkage, labels=features.columns.to_list(), ax=ax1, leaf_rotation=90
+)
+dendro_idx = np.arange(0, len(dendro["ivl"]))
+
+ax2.imshow(corr[dendro["leaves"], :][:, dendro["leaves"]])
+ax2.set_xticks(dendro_idx)
+ax2.set_yticks(dendro_idx)
+ax2.set_xticklabels(dendro["ivl"], rotation="vertical")
+ax2.set_yticklabels(dendro["ivl"])
+_ = fig.tight_layout()
+     
+plt.savefig("/mnt/storage2/users/ahnelll1/master_thesis/NeoPrioProject/machine_learning/feature_correlation.png")  
+
+# Feature Importances
+result = permutation_importance(
+    rf_classifier, test_features, test_labels, n_repeats=10, random_state=42, n_jobs=2
+)
+
+for i in result.importances_mean.argsort()[::-1]:
+    if result.importances_mean[i] - 2 * result.importances_std[i] > 0:
+        print(f"{feature_list[i]:<8}"
+              f"{result.importances_mean[i]:.3f}"
+              f" +/- {result.importances_std[i]:.3f}")
+              
+importances_mean = result.importances_mean
+importances_std = result.importances_std
+
+# Adjust the importance scores by dividing mean by std (to penalize high variability)
+adjusted_importances = importances_mean / (importances_std + 1e-10)  # Adding a small value to avoid division by zero
+
+# Normalize the adjusted importances to get final weights
+feature_weights_adjusted = adjusted_importances / adjusted_importances.sum()
+
+# Display the adjusted feature weights
+for feature, weight in zip(features, feature_weights_adjusted):
+    print(f"{feature}: Adjusted Weight = {weight:.4f}")
+
+sorted_importances_idx = result.importances_mean.argsort()
+importances = pd.DataFrame(
+    result.importances[sorted_importances_idx].T,
+    columns=features.columns[sorted_importances_idx],
+)
+ax = importances.plot.box(vert=False, whis=10)
+ax.set_title("Permutation Importances (test set)")
+ax.axvline(x=0, color="k", linestyle="--")
+ax.set_xlabel("Decrease in accuracy score")
+ax.figure.tight_layout()
+
+plt.savefig("/mnt/storage2/users/ahnelll1/master_thesis/NeoPrioProject/machine_learning/rf_feature_importances_test.png")
+
+result = permutation_importance(
+    rf_classifier, train_features, train_labels, n_repeats=10, random_state=42, n_jobs=2
+)
+
+sorted_importances_idx = result.importances_mean.argsort()
+importances = pd.DataFrame(
+    result.importances[sorted_importances_idx].T,
+    columns=features.columns[sorted_importances_idx],
+)
+ax = importances.plot.box(vert=False, whis=10)
+ax.set_title("Permutation Importances (train set)")
+ax.axvline(x=0, color="k", linestyle="--")
+ax.set_xlabel("Decrease in accuracy score")
+ax.figure.tight_layout()
+
+plt.savefig("/mnt/storage2/users/ahnelll1/master_thesis/NeoPrioProject/machine_learning/rf_feature_importances_train.png")
+
+result = permutation_importance(
+    clf, test_features, test_labels_int, n_repeats=10, random_state=42, n_jobs=2
+)
+
+sorted_importances_idx = result.importances_mean.argsort()
+importances = pd.DataFrame(
+    result.importances[sorted_importances_idx].T,
+    columns=features.columns[sorted_importances_idx],
+)
+ax = importances.plot.box(vert=False, whis=10)
+ax.set_title("Permutation Importances (test set)")
+ax.axvline(x=0, color="k", linestyle="--")
+ax.set_xlabel("Decrease in accuracy score")
+ax.figure.tight_layout()
+
+plt.savefig("/mnt/storage2/users/ahnelll1/master_thesis/NeoPrioProject/machine_learning/lasso_feature_importances_test.png")
+
+result = permutation_importance(
+    clf, train_features, train_labels_int, n_repeats=10, random_state=42, n_jobs=2
+)
+
+sorted_importances_idx = result.importances_mean.argsort()
+importances = pd.DataFrame(
+    result.importances[sorted_importances_idx].T,
+    columns=features.columns[sorted_importances_idx],
+)
+ax = importances.plot.box(vert=False, whis=10)
+ax.set_title("Permutation Importances (train set)")
+ax.axvline(x=0, color="k", linestyle="--")
+ax.set_xlabel("Decrease in accuracy score")
+ax.figure.tight_layout()
+
+plt.savefig("/mnt/storage2/users/ahnelll1/master_thesis/NeoPrioProject/machine_learning/lasso_feature_importances_train.png")
+
+
 # Test Set
-test_patients = test_features[:, 0]
-test_features = test_features[:, 1:]
 pred_proba = rf_classifier.predict_proba(test_features)[:, 1]
 pred_labels = (pred_proba > 0.5).astype(int)
-test_labels = [0 if label == 'N' else 1 for label in test_labels]
 
-accuracy = accuracy_score(test_labels, pred_labels)
-precision = precision_score(test_labels, pred_labels)
-recall = recall_score(test_labels, pred_labels)
-f1 = f1_score(test_labels, pred_labels)  
-auc = roc_auc_score(test_labels, pred_proba)
+precision = precision_score(test_labels_int, pred_labels)
+recall = recall_score(test_labels_int, pred_labels)
+f1 = f1_score(test_labels_int, pred_labels)  
+auc = roc_auc_score(test_labels_int, pred_proba)
 
-print("Accuracy:", accuracy)
+print(f"RF train accuracy: {rf_classifier.score(train_features, train_labels):.3f}")
+print(f"RF test accuracy: {rf_classifier.score(test_features, test_labels):.3f}")
 print("Precision:", precision)
 print("Recall:", recall)
 print("F1-score:", f1)
 print("AUC:", auc) 
+
+pred_proba = clf.predict(test_features)
+pred_labels = (pred_proba > 0.5).astype(int)
+
+precision = precision_score(test_labels_int, pred_labels)
+recall = recall_score(test_labels_int, pred_labels)
+f1 = f1_score(test_labels_int, pred_labels)  
+auc = roc_auc_score(test_labels_int, pred_proba)
+
+print(f"LASSO train accuracy: {clf.score(train_features, train_labels_int):.3f}")
+print(f"LASSO test accuracy: {clf.score(test_features, test_labels_int):.3f}")
+print("Precision:", precision)
+print("Recall:", recall)
+print("F1-score:", f1)
+print("AUC:", auc) 
+
 
 
 df = pd.DataFrame({'patientIdentifier': test_patients, 'pred_proba': pred_proba})
--- rank_sum_algorithm/neoantigen_prioritization_rank_sum.py
+++ rank_sum_algorithm/neoantigen_prioritization_rank_sum.py
@@ -10,110 +10,149 @@ import bisect
 def calc_qscore(row, columns, number_of_quantiles):
     qscore_series = row[[s for s in columns if s.endswith('_qscore')]]
     return qscore_series.sum(skipna=True) / (number_of_quantiles * (~qscore_series.isnull()).sum())
-    
-def rank_sum(pvacseq_filename, neofox_filename):  
+
+
+def rank_sum(pvacseq_filename, neofox_filename):
     pvacseq_df = pd.read_csv(pvacseq_filename, sep="\t", header=0)
     neofox_df = pd.read_csv(neofox_filename, sep="\t", header=0)
-    
-    f = open('/mnt/storage2/users/ahnelll1/master_thesis/NeoPrioProject/rank_sum_algorithm/data/quantiles_25.json')
-    features = json.load(f)
-    f.close()
-    
+
     if neofox_df.shape[0] != pvacseq_df.shape[0]:
         raise ValueError("Neofox output and pVACseq output should have the same amount of rows.")
-    annotation_df = neofox_df.merge(right=pvacseq_df.loc[:, ['Chromosome', 'Start', 'Stop', 'Transcript', 'Variant Type', 
-    'MHCflurry WT IC50 Score', 'MHCflurry MT IC50 Score', 'MHCflurry WT Percentile', 'MHCflurry MT Percentile', 
-    'NetMHC WT IC50 Score', 'NetMHC MT IC50 Score', 'NetMHC WT Percentile', 'NetMHC MT Percentile', 
-    'NetMHCpan WT IC50 Score', 'NetMHCpan MT IC50 Score', 'NetMHCpan WT Percentile',  'NetMHCpan MT Percentile', 
-    'Predicted Stability', 'Half Life', 'Stability Rank', 'NetMHCstab allele']], on=['Chromosome', 'Start', 'Stop', 'Transcript'])
-    
+    annotation_df = neofox_df.merge(
+        right=pvacseq_df.loc[:, ['Chromosome', 'Start', 'Stop', 'Transcript', 'Variant Type',
+                                 'MHCflurry WT IC50 Score', 'MHCflurry MT IC50 Score', 'MHCflurry WT Percentile',
+                                 'MHCflurry MT Percentile',
+                                 'NetMHC WT IC50 Score', 'NetMHC MT IC50 Score', 'NetMHC WT Percentile',
+                                 'NetMHC MT Percentile',
+                                 'NetMHCpan WT IC50 Score', 'NetMHCpan MT IC50 Score', 'NetMHCpan WT Percentile',
+                                 'NetMHCpan MT Percentile',
+                                 'Predicted Stability', 'Half Life', 'Stability Rank', 'NetMHCstab allele']],
+        on=['Chromosome', 'Start', 'Stop', 'Transcript'])
+
     # Prefilter (weighted average below 500)
-    annotation_df_prefiltered = annotation_df[(annotation_df['MHCflurry MT IC50 Score'] * 0.4 + annotation_df['NetMHCpan MT IC50 Score'] * 0.4 + annotation_df['NetMHC MT IC50 Score'] * 0.2) < 500]
-    
-    output_df = annotation_df_prefiltered.loc[:, ['Transcript', 'Chromosome', 'Start', 'Stop'] + [f for f in features]]
-    
-    output_df = ranking(annotation_df_prefiltered, output_df, features)
-    
-    output_df.to_csv(os.path.join(os.path.abspath(os.path.dirname(os.path.dirname(neofox_filename))), 'rank_sum_out.tsv'), sep='\t', index=False, header=True)
-    
-def rank_sum_training_data(neofox_filename):  
+    annotation_df_prefiltered = annotation_df[(annotation_df['MHCflurry MT IC50 Score'] * 0.4 + annotation_df[
+        'NetMHCpan MT IC50 Score'] * 0.4 + annotation_df['NetMHC MT IC50 Score'] * 0.2) < 500]
+
+    output_df = ranking(annotation_df_prefiltered, ['Transcript', 'Chromosome', 'Start', 'Stop'])
+
+    output_df.to_csv(
+        os.path.join(os.path.abspath(os.path.dirname(os.path.dirname(neofox_filename))), 'rank_sum_out.tsv'), sep='\t',
+        index=False, header=True)
+
+
+def rank_sum_training_data(neofox_filename):
     neofox_df = pd.read_csv(neofox_filename, sep="\t", header=0)
-    
+
+    output_df = ranking(neofox_df, ['patientIdentifier'])
+
+    output_df.to_csv(os.path.join(os.path.abspath(os.path.dirname(neofox_filename)),
+                                  os.path.splitext(os.path.basename(neofox_filename))[0] + '_rank_sum_out_25.tsv'),
+                     sep='\t', index=False, header=True)
+
+
+def ranking(input_df, meta_columns):
     f = open('/mnt/storage2/users/ahnelll1/master_thesis/NeoPrioProject/rank_sum_algorithm/data/quantiles_25.json')
     features = json.load(f)
     f.close()
-    
-    output_df = neofox_df.loc[:, ['patientIdentifier'] + [f for f in features if f in neofox_df.columns]]
 
-    output_df = ranking(neofox_df, output_df, features)
-    
-    output_df.to_csv(os.path.join(os.path.abspath(os.path.dirname(neofox_filename)), os.path.splitext(os.path.basename(neofox_filename))[0] + '_rank_sum_out_25.tsv'), sep='\t', index=False, header=True)
+    f = open('/mnt/storage2/users/ahnelll1/master_thesis/NeoPrioProject/machine_learning/weights.json')
+    weights = json.load(f)
+    f.close()
 
-def ranking(input_df, output_df, features):
+    output_df = input_df.loc[:, meta_columns + [f for f in features if f]]
+
     number_of_quantiles = None
     for feature_name in features:
         if feature_name not in output_df.columns:
             continue
         index = output_df.columns.get_loc(feature_name)
         number_of_quantiles = len(features[feature_name]['quantiles'])
-        
+
         # Ranking
         column_name_rank = feature_name + '_rank'
         if feature_name == 'Selfsimilarity_conserved_binder':
             # if Selfsimilarity is NaN, the DAI is almost zero -> NaN would be preferable
-            rank_column = output_df[feature_name].rank(method='max', ascending=features[feature_name]['direction'] == 'lower', na_option='top')
+            rank_column = output_df[feature_name].rank(method='max',
+                                                       ascending=features[feature_name]['direction'] == 'lower',
+                                                       na_option='top')
         elif feature_name == 'imputedGeneExpression':
             output_df.insert(index, 'rnaExpression', input_df['rnaExpression'])
             index += 1
             # if RNA data given then measured RNA expression should be used, else the imputed
-            rank_column = output_df['rnaExpression'].where(~output_df['rnaExpression'].isnull(), output_df['imputedGeneExpression']).rank(method='max', ascending=features[feature_name]['direction'] == 'lower', na_option='bottom')             
+            rank_column = output_df['rnaExpression'].where(~output_df['rnaExpression'].isnull(),
+                                                           output_df['imputedGeneExpression']).rank(method='max',
+                                                                                                    ascending=features[
+                                                                                                                  feature_name][
+                                                                                                                  'direction'] == 'lower',
+                                                                                                    na_option='bottom')
             column_name_rank = 'gene_expression_rank'
         elif feature_name == 'Priority_score_imputed_fromDNA' or feature_name == 'Priority_score_imputed_fromRNA':
-            output_df.insert(index, feature_name.replace('_imputed', ''), input_df[feature_name.replace('_imputed', '')])
+            output_df.insert(index, feature_name.replace('_imputed', ''),
+                             input_df[feature_name.replace('_imputed', '')])
             index += 1
             # if RNA data given then measured RNA expression should be used for priority score, else the imputed            
-            rank_column = output_df[feature_name.replace('_imputed', '')].where(~output_df[feature_name.replace('_imputed', '')].isnull(), output_df[feature_name]).rank(method='max', ascending=features[feature_name]['direction'] == 'lower', na_option='bottom')            
+            rank_column = output_df[feature_name.replace('_imputed', '')].where(
+                ~output_df[feature_name.replace('_imputed', '')].isnull(), output_df[feature_name]).rank(method='max',
+                                                                                                         ascending=
+                                                                                                         features[
+                                                                                                             feature_name][
+                                                                                                             'direction'] == 'lower',
+                                                                                                         na_option='bottom')
             column_name_rank = feature_name.replace('_imputed', '') + '_rank'
         else:
-            rank_column = output_df[feature_name].rank(method='max', ascending=features[feature_name]['direction'] == 'lower', na_option='bottom')
+            rank_column = output_df[feature_name].rank(method='max',
+                                                       ascending=features[feature_name]['direction'] == 'lower',
+                                                       na_option='bottom')
         output_df.insert(index + 1, column_name_rank, rank_column)
-            
+
         # Quantile Score
         column_name_qscore = feature_name + '_qscore'
         if feature_name == 'imputedGeneExpression':
             # if RNA data given then measured RNA expression should be used, else the imputed
-            quantile_score_column = [bisect.bisect_left(features[feature_name]['quantiles'], num) if not np.isnan(num) else None for _, num in output_df['rnaExpression'].where(~output_df['rnaExpression'].isnull(), output_df['imputedGeneExpression']).items()]
+            quantile_score_column = [
+                bisect.bisect_left(features[feature_name]['quantiles'], num) if not np.isnan(num) else None for _, num
+                in output_df['rnaExpression'].where(~output_df['rnaExpression'].isnull(),
+                                                    output_df['imputedGeneExpression']).items()]
             column_name_qscore = 'gene_expression_qscore'
         elif feature_name == 'Priority_score_imputed_fromDNA' or feature_name == 'Priority_score_imputed_fromRNA':
             # if RNA data given then measured RNA expression should be used for priority score, else the imputed  
-            quantile_score_column = [bisect.bisect_left(features[feature_name]['quantiles'], num) if not np.isnan(num) else None for _, num in output_df[feature_name.replace('_imputed', '')].where(~output_df[feature_name.replace('_imputed', '')].isnull(), output_df[feature_name]).items()]            
+            quantile_score_column = [
+                bisect.bisect_left(features[feature_name]['quantiles'], num) if not np.isnan(num) else None for _, num
+                in output_df[feature_name.replace('_imputed', '')].where(
+                    ~output_df[feature_name.replace('_imputed', '')].isnull(), output_df[feature_name]).items()]
             column_name_qscore = feature_name.replace('_imputed', '') + '_qscore'
         else:
-            quantile_score_column = [bisect.bisect_left(features[feature_name]['quantiles'], num) if not np.isnan(num) else None for _, num in output_df[feature_name].items()]
-        quantile_score_column = quantile_score_column if features[feature_name]['direction'] == 'upper' else [number_of_quantiles - x if not pd.isnull(x) else None for x in quantile_score_column]
+            quantile_score_column = [
+                bisect.bisect_left(features[feature_name]['quantiles'], num) if not np.isnan(num) else None for _, num
+                in output_df[feature_name].items()]
+        quantile_score_column = quantile_score_column if features[feature_name]['direction'] == 'upper' else [
+            number_of_quantiles - x if not pd.isnull(x) else None for x in quantile_score_column]
         output_df.insert(index + 2, column_name_qscore, quantile_score_column)
-      
+
     # Output  
-    output_df['rank_sum'] = output_df[[s for s in list(output_df.columns) if s.endswith('_rank')]].sum(axis=1, skipna=False)
+    output_df['rank_sum'] = output_df[[s for s in list(output_df.columns) if s.endswith('_rank')]].sum(axis=1,
+                                                                                                       skipna=False)
     output_df['qscore'] = None
     if not output_df.shape[0] == 0:
-        output_df['qscore'] = output_df.apply(calc_qscore, axis='columns', args=(list(output_df.columns), number_of_quantiles))
+        output_df['qscore'] = output_df.apply(calc_qscore, axis='columns',
+                                              args=(list(output_df.columns), number_of_quantiles))
 
     output_df = output_df.sort_values(by='rank_sum')
-    
+
     return output_df
-    
+
 
 def main(argv):
     usage = "usage: python neoantigen_prioritization.py "
     desc = "Prioritizes neoantigens based on neofox features with a rank sum algorithm."
     parser = OptionParser(usage=usage, description=desc)
-    parser.add_option("--pvacseq-filtered-output", action="store", dest="pvacseq_filtered_output", help="Filtered TSV file output of pVACseq")
+    parser.add_option("--pvacseq-filtered-output", action="store", dest="pvacseq_filtered_output",
+                      help="Filtered TSV file output of pVACseq")
     parser.add_option("--neofox-output", action="store", dest="neofox_output", help="TSV file output of neofox")
     (options, args) = parser.parse_args()
-    
+
     rank_sum(options.pvacseq_filtered_output, options.neofox_output)
 
-    
+
 if __name__ == "__main__":
     main(sys.argv)
